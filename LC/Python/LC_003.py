cf81c2bb7ee063931f9b6c27280c6a3cefef32fd
Ken <kenkjw@gmail.com>
2016-11-14 18:24:37 -0800

    Add LC_003

---
+++ LC/Python/LC_003.py
@@ -0,0 +1,28 @@
+""" 003 - Longest Substring Without Repeating Characters
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+Given a string, find the length of the longest substring without repeating characters.
+
+Examples:
+
+Given "abcabcbb", the answer is "abc", which the length is 3.
+
+Given "bbbbb", the answer is "b", with the length of 1.
+
+Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+"""
+
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        max = s[0:0]
+        str = ""
+        for c in s:
+            str = str[str.find(c) + 1:] + c
+            if len(str) > len(max):
+                max = str
+        return len(max)
